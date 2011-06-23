8225634481bd5f9d5dcd5b125e0c9f775bbaf185
anymoto <ana@castromartinez.com>
2011-06-23 18:18:00 -0500

    fix specs

--- spec/ticketmaster-codaset_spec.rb
+++ spec/ticketmaster-codaset_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Codaset" do
-  it "should be able to instantiate a new instance" do
-    
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      ACCESS_TOKEN = { "access_token" => "01234567890abcdef", "refresh_token" => "01234567890abcdef", "expires_in" => 1209600, "username" => "anymoto" } 
+
+  before (:all) do
+     stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      #ACCESS_TOKEN = { "access_token" => "01234567890abcdef", "refresh_token" => "01234567890abcdef", "expires_in" => 1209600, "username" => "anymoto" } 
       stub.post('/authorization/token') { [200, {}, ACCESS_TOKEN.to_json] }
     end
 
@@ -16,7 +16,14 @@ describe "Ticketmaster::Provider::Codaset" do
     end
     
     @ticketmaster = TicketMaster.new(:codaset, {:username => 'anymoto', :password => '000000', :client_id => '07f16ec7', :client_secret => '442fe0b16'})
+  end
+
+  it "should be able to instantiate a new instance" do
     @ticketmaster.should be_an_instance_of(TicketMaster)
     @ticketmaster.should be_a_kind_of(TicketMaster::Provider::Codaset)
+  end
+
+  it "should return true with valid authentication" do
+    @ticketmaster.valid?.should be_true
   end
 end
